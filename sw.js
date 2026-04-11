c63b1c5989ed442a1267decfc10a5af847524478
VentasShop <89612569+Spetsnaz74@users.noreply.github.com>
2026-04-11 13:50:34 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'ofertas-v1';
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => response)
-      .catch(() => caches.match(event.request))
-  );
-});
